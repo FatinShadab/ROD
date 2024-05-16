02f814d10b11332f658d751ee193c1601fd5bc27
FatinShadab <macrotech21@gmail.com>
2024-05-16 23:44:36 +0600

    Configured camera and base app to the project

--- src/core/urls.py
+++ src/core/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
-    path('model/v1', include('rodMv1.urls'))
+    path('camera/', include('camera.urls')),
+    path('model/v1/', include('rodMv1.urls')),
 ]
