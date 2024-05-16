b5f33d686d5928afbff2c1eafbedf21f3e7f69bc
FatinShadab <macrotech21@gmail.com>
2024-05-16 23:02:08 +0600

    Update urls.py
    added login, logout, signup, dashboard url and removed camera related url

--- src/base/urls.py
+++ src/base/urls.py
@@ -3,6 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', home, name="home_view"),
-    path('camera-register', camera_register, name="camera_register_view"),
-    path('camera-test', camera_test, name="camera_test_view"),
+    path('login', login_view, name="login_view"),
+    path('sign_up', sign_up, name="sign_up_view"),
+    path('dashboard', user_dashboard, name="dashboard_view"),
+    path('logout', logout_view, name="logout_view")
 ]
