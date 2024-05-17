0931080cb3f59388159b2ca3ae112e93698fccd7
FatinShadab <macrotech21@gmail.com>
2024-05-17 16:44:47 +0600

    AddedPassResetForm

--- src/base/forms.py
+++ src/base/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import PasswordResetForm
+
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(
@@ -43,7 +45,7 @@ class SignUpForm(UserCreationForm):
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
-        if User.objects.filter(email=email).exists():
+        if User.objects.filter(email=email, is_active=True).exists():
             raise forms.ValidationError("This email is already registered.")
         return email
 
@@ -51,3 +53,26 @@ class SignUpForm(UserCreationForm):
 class UserLoginForm(forms.Form):
     username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control form-control-lg', 'type': 'text', 'id': 'form3Example1cg'}))
     password = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control form-control-lg', 'type': 'password', 'id': 'form3Example4cg'}))
+
+
+class PassResetForm(PasswordResetForm):
+    email = forms.EmailField(
+        required=True, 
+        help_text="Required. Provide a valid email address.",
+        widget=forms.EmailInput(attrs={
+            'placeholder': 'Your Email',
+            'type': 'email',
+            'id': 'form3Example3cg',
+            'class': 'form-control form-control-lg'
+        })
+    )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not User.objects.filter(email=email, is_active=True).exists():
+            raise forms.ValidationError("The email address you entered is not registered. Please enter registered email id")
+        return email
+    
+    def get_user(self):
+        email = self.cleaned_data.get('email')
+        return User.objects.get(email=email, is_active=True)
