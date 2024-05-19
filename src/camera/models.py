ba239f4824f088acaeaabb97471d70dcba0c4185
FatinShadab <macrotech21@gmail.com>
2024-05-19 12:45:06 +0600

    Added camera model for storing camera data

--- src/camera/models.py
+++ src/camera/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Camera(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cameras')
+    cname = models.CharField(max_length=255)  # Name of the camera
+    cip = models.GenericIPAddressField(protocol='both', unpack_ipv4=False)  # IP address of the camera
+    cport = models.PositiveIntegerField()  # Port number
+    selected = models.BooleanField(default=False)  # If selected, others should be false
+
+    def save(self, *args, **kwargs):
+        if self.selected:
+            # Ensure only one camera is selected per user
+            Camera.objects.filter(user=self.user, selected=True).update(selected=False)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.cname} ({self.cip}:{self.cport}) - {'Selected' if self.selected else 'Not Selected'}"
