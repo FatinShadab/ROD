ff38ab808b46c517e3608cdab5d807afb8b3b496
FatinShadab <macrotech21@gmail.com>
2024-05-19 11:45:43 +0600

    Update urls.py

--- src/base/urls.py
+++ src/base/urls.py
@@ -3,8 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('', home, name="home_view"),
+    path('dashboard', user_dashboard, name="dashboard_view"),
     path('login', login_view, name="login_view"),
     path('sign_up', sign_up, name="sign_up_view"),
-    path('dashboard', user_dashboard, name="dashboard_view"),
-    path('logout', logout_view, name="logout_view")
+    path('logout', logout_view, name="logout_view"),
+    path('pass_reset/', password_reset_request_view, name='password_reset_view'),
+    path('pass_reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm_view'),
+    path('pass_reset/done/', CustomPasswordResetCompleteView.as_view(), name='password_reset_complete_view'),
 ]
