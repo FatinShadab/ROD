8f2c06920745b3cf6bc5e5803b42bdb80e49f8e7
FatinShadab <macrotech21@gmail.com>
2024-05-20 18:50:16 +0600

    Update cam.py

--- src/camera/cam.py
+++ src/camera/cam.py
@@ -3,15 +3,19 @@ import cv2
 class IPCamera:
     def __init__(self, video_source):
         self.video = cv2.VideoCapture(video_source)
+        #self.video = cv2.VideoCapture(0)
+        if not self.video.isOpened():
+            raise ValueError("Failed to open video source")
 
     def __del__(self):
-        self.video.release()
+        if self.video.isOpened():
+            print("Done")
+            self.video.release()
 
     def get_frame(self):
         success, frame = self.video.read()
-        if not success:
+        if success:
+            _, jpeg = cv2.imencode('.jpg', frame)
+            return jpeg.tobytes()
+        else:
             return None
-        
-        _, jpeg = cv2.imencode('.jpg', frame)
-        
-        return jpeg.tobytes()
