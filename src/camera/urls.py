ed74e601da4f3e927c38e7f6994c57877e579052
FatinShadab <macrotech21@gmail.com>
2024-05-20 18:51:16 +0600

    Update urls.py

--- src/camera/urls.py
+++ src/camera/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('test', camera_test, name="camera_test_view"),
+    path('test/<str:cname>', camera_test, name="camera_test_view"),
     path('register', camera_register, name="camera_register_view"),
 ]
