99b98b5e7a5eb28c85d53e9a18ab3ca048927e53
FatinShadab <macrotech21@gmail.com>
2024-05-20 18:53:33 +0600

    Update views.py
    * Modified the gen() function
    * completed camera_register and camera_test view

--- src/camera/views.py
+++ src/camera/views.py
@@ -1,22 +1,49 @@
-from django.shortcuts import render
-from django.http import StreamingHttpResponse
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404
+from django.http import StreamingHttpResponse, HttpResponse
 from django.contrib.auth.decorators import login_required
 
+from .forms import CameraRegForm
+from .models import Camera
 from .cam import IPCamera
 
+
 def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        if frame is None:
-            break
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    try:
+        while True:
+            frame = camera.get_frame()
+            if frame is None:
+                break
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpg\r\n\r\n' + frame + b'\r\n')
+    except Exception as e:
+        print(f"Error: {e}")
+        yield b''
 
 @login_required(login_url='login_view')
 def camera_register(request):
-    return render(request, "cam.html")
+    if request.method ==  "POST":
+        reg_form = CameraRegForm(request.POST)
+        if reg_form.is_valid():
+            cam_data = reg_form.save(commit=False)
+            cam_data.selected = False
+            cam_data.user = request.user
+            cam_data.save()
+            messages.success(request, "Camera registered successfully.")
+        
+    cameras = Camera.objects.filter(user=request.user)
+    reg_form = CameraRegForm()
+
+    return render(request, "cam.html", {'form':reg_form, 'cameras': cameras})
 
 @login_required(login_url='login_view')
-def camera_test(request):
-    video_source = 'http://192.168.0.100:8080/video'
-    return StreamingHttpResponse(gen(IPCamera(video_source)), content_type='multipart/x-mixed-replace; boundary=frame')
+def camera_test(request, cname):
+    testing_camera = get_object_or_404(Camera, cname=cname)
+    video_source = f'http://{testing_camera.cip}:{testing_camera.cport}/video'
+
+    try:
+        camera = IPCamera(video_source)
+        frame = gen(camera)
+        return StreamingHttpResponse(frame, content_type='multipart/x-mixed-replace; boundary=frame')
+    except Exception as e:
+        return HttpResponse(f"Failed to load video stream: {e}", status=503)
