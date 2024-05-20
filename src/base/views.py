d0d68b59de28c946817c5c8df7a0f28dcc176057
FatinShadab <macrotech21@gmail.com>
2024-05-20 18:54:39 +0600

    Update views.py
    Added backend logic for password reset functionality

--- src/base/views.py
+++ src/base/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import SignUpForm, UserLoginForm
+from django.urls import reverse_lazy
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.views import PasswordResetConfirmView, PasswordResetCompleteView
+
+from django.conf import settings
+from .forms import SignUpForm, UserLoginForm, PassResetForm, CustomSetPasswordForm
+
 
 # Create your views here.
 def home(request):
@@ -15,7 +25,7 @@ def sign_up(request):
             user = form.save()
             login(request, user)
             return redirect('home_view')
-        messages.error(request, "Unsuccessful registration. Invalid information.")
+        messages.error(request, "<span style='font-size: 22px;'>👮</span>: Unsuccessful registration. Invalid information.")
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
@@ -31,11 +41,45 @@ def login_view(request):
                 login(request, user)
                 return redirect('home_view')
             else:
-                messages.error(request, "Login Unsuccessful. Invalid information.")
+                messages.error(request, "<span style='font-size: 22px;'>👮</span>: Login Unsuccessful. Invalid information.")
     else:
         form = UserLoginForm()
 
     return render(request, 'login.html', {'form': form})
+
+def password_reset_request_view(request):
+    if request.method == "POST":
+        password_reset_form = PassResetForm(request.POST)
+        if password_reset_form.is_valid():
+            email = password_reset_form.cleaned_data['email']
+            user = password_reset_form.get_user() # Generate a password reset token
+            token = default_token_generator.make_token(user) # Generate uid based on user id
+            uid = urlsafe_base64_encode(str(user.pk).encode()) # Build the password reset link
+            protocol = 'https' if request.is_secure() else 'http'
+            domain = get_current_site(request).domain
+            reset_link = reverse_lazy('password_reset_confirm_view', kwargs={'uidb64': uid, 'token': token,})
+            reset_email_subject = 'Password Reset Request Response @R.O.D' # Send the password reset email
+            reset_email_body = render_to_string('password_reset_email.html', {'reset_link': reset_link, 'protocol': protocol, 'domain': domain})
+            
+            try:
+                send_mail(
+                    reset_email_subject,
+                    None,
+                    settings.EMAIL_HOST_USER,
+                    [email],
+                    fail_silently=False,
+                    html_message=reset_email_body
+                )
+                messages.success(request, "<span style='font-size: 22px;'>👮</span>: Please Check Your Email For Reset Link.")
+            except Exception as e:
+                print(e)
+                messages.error(request, "<span style='font-size: 26px;'>👮</span>: Internal Server Error Please Try Again Later !")
+        else:
+             messages.error(request, "<span style='font-size: 26px;'>👮</span>: Didn't Find Any Active Account With This Email !")
+    
+    password_reset_form = PassResetForm()
+
+    return render(request, 'forgetpass.html', {'form': password_reset_form})
 
 @login_required(login_url='login_view')
 def logout_view(request):
@@ -45,3 +89,13 @@ def logout_view(request):
 @login_required(login_url='login_view')
 def user_dashboard(request):
     return render(request, "dashboard.html")
+
+
+class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+    template_name = 'password_reset_confirm.html'
+    form_class = CustomSetPasswordForm
+    success_url = reverse_lazy('password_reset_complete_view')
+
+
+class CustomPasswordResetCompleteView(PasswordResetCompleteView):
+    template_name = 'pass_reset_complete.html'
