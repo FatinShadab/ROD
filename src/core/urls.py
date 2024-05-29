b1a99e9dfa632e0cad16f2422ac32664af41b358
FatinShadab <macrotech21@gmail.com>
2024-05-29 22:22:03 +0600

    configured odcm app and app url

--- src/core/urls.py
+++ src/core/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
-    path('camera/', include('camera.urls'))
+    path('camera/', include('camera.urls')),
+    path('service/v0', include('odcm.urls')),
 ]
